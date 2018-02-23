5d94543033f1af3f6edbe7f53d1908de1faf0d13
John Paton <john@johnpaton.net>
2018-02-23 21:11:00 +0100

    javascript cleanup

--- js/app.js
+++ js/app.js
@@ -1,62 +1,113 @@
-// Under contruction message
-var constructionAlert = "<div class=\"alert alert-danger show mt-5\" role=\"alert\">"
-    + "<strong>This page is still under construction.</strong> It may contain bugs and/or errors. Don't take the info seriously!"
-    + "</div>"
+////// Options
 
-$('.container-fluid .anchor').after(constructionAlert)
+var underConstruction = true;
+var rsvpDisabled = false;
+var rsvpSubmissionEmail = 'patonkoning2018@gmail.com';
 
-// Smooth scrolling
-// https://www.w3schools.com/bootstrap/bootstrap_ref_js_scrollspy.asp
-// Add smooth scrolling on all links inside the navbar on desktop
-$("#navbar a").on('click', function(event) {
-    // Prevent default anchor click behavior
-    event.preventDefault();
 
-    // store offset from mobile navbar and collapse if expanded
-    var offs
-    if ( $('.collapse').is('.show') ) {
-        offs = $('#navbarSupportedContent').height();
+////// Util functions
 
-        // collapse the menu on click
-        $('.collapse').collapse('hide');
+function mobileNavExpanded(){
+    // Use this instead of $('.cover').is('.visible') since this is true even
+    //     during the collapse animation. False on desktop screens.
+    return ( $('.navbar-toggler').is(':visible') 
+             && $('#navbarSupportedContent').is(':visible') );
+};
 
+
+function mobileNavOffset() {
+    // Extra scroll position recorded due to mobile nav content being visible
+    if ( mobileNavExpanded() ) {
+        return $('#navbarSupportedContent').height();
     } else {
-        offs = 0
+        return 0;
     };
+};
 
-    // Store hash
-    var hash = this.hash;
 
-    // animate scroll
-    if ( hash != "" ) {
-        console.log('Scrolling to '+hash)
+function mobileNavCollapse() {
+    // Close the mobile navbar if it's expanded
+    if ( mobileNavExpanded() ) {
+        $('.collapse').collapse('hide');
+    };
+};
+
+
+function scrollToAnchor(hash, offset){
+    // Smoothly scroll to an anchor on the page
+    // See https://www.w3schools.com/bootstrap/bootstrap_ref_js_scrollspy.asp
+    if ( hash != '' ) {
+        console.log('Scrolling to ' + hash)
         // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        // The optional number (800) specifies the number of milliseconds it 
+        //    takes to scroll to the specified area
         $('html, body').animate({
-          scrollTop: $(hash).offset().top - offs
-        }, 500, function(){
+          scrollTop: $(hash).offset().top - offset
+        }, 
+        500, 
+        function(){
             // Add hash (#) to URL when done scrolling (default click behavior)
             window.location.hash = hash;
         });
     };
+};
+
+
+////// Style adjustments
+
+// highlight language link for current page
+$(".lang-link").each(function(){
+    if ( this.pathname == window.location.pathname ){
+        console.log('Highlighting lang-link '+this.innerHTML)
+        $(this).addClass('text-light')
+    };
 });
+
+if (rsvpDisabled) {
+    $('#rsvpSubmit').attr('disabled', true)
+                    .after("<span class=\"text-danger\"> (disabled until the invites go out!)</span>");
+
+};
+
+
+////// Content insertions
+
+// Under contruction message (option: underConstruction)
+var constructionAlert = "<div class=\"alert alert-danger show mt-5\" role=\"alert\">"
+    + "<strong>This page is still under construction.</strong> It may contain bugs and/or errors."
+    + "Don't take the info seriously! </div>"
+if (underConstruction) {
+    $('.container-fluid .anchor').after(constructionAlert)
+};
+
+
+////// Functionality
+
+// Smooth scrolling on all links in the navbar (including the brand)
+$("#navbar a").on('click', function(event) {
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // store offset from mobile navbar, collapse if expanded, and scroll to hash
+    var offset = mobileNavOffset();
+    mobileNavCollapse();
+    scrollToAnchor(this.hash, offset)
+});
+
 
 // Maintain position when loading other language page
 // Save the scroll position if clicking the language link
 $(".lang-link").on('click', function(e){
     e.preventDefault();
-    // store offset from mobile navbar (collapse may already be triggered so don't use collapse.is(show))
-    var offs
-    if ( $('.navbar-toggler').is(':visible') && $('#navbarSupportedContent').is(':visible') ) {
-        offs = $('#navbarSupportedContent').height();
-    } else {
-        offs = 0
-    };
+    // store offset from mobile navbar
+    var offs = mobileNavOffset();
 
     // store location if we are changing pages in sessionStorage
     if ( this.pathname != window.location.pathname ){
         console.log('Storing scroll position')
         sessionStorage.setItem('scrollPos', $(window).scrollTop() - offs);
+
+        // load page for target language
         window.location.href = this.href;
     };
 });
@@ -71,35 +122,13 @@ $(document).ready(function(){
 });
 
 
-// highlight language link for current page
-$(".lang-link").each(function(){
-    if ( this.pathname == window.location.pathname ){
-        console.log('Highlighting lang-link '+this.innerHTML)
-        $(this).addClass('text-light')
-    };
-});
-
-
-// collapse mobile navbar if clicking off it
-$('body').on('click', function(e){
-    // check that:
-    //    * navbar is expanded
-    //    * we're on a small screen
-    //    * the click was not on the navbar
-    if ( $('.navbar-toggler').is(':visible')  && $('.collapse').is('.show')
-         && ($(e.target).parents('#navbar').length == 0) ) {
-        console.log('Click off navbar, collapsing')
-        $('.collapse').collapse('hide')
-    };
-});
-
-// Form Validation
+////// Form Validation
 
 // Require group size only if accepting invite
 // https://stackoverflow.com/a/11967638
 $('#rsvpStatus').change(function() {
-    var opt = $('#rsvpStatus option:selected')[0].innerHTML;
-    if (opt == "Regret") {
+    var selection = $('#rsvpStatus option:selected')[0].innerHTML;
+    if (selection == "Regret") {
         console.log('Regret, disabling fields')
         $('#rsvpNumber').attr('required', false);
         $('#rsvpNumber').attr('disabled', true);
@@ -110,29 +139,32 @@ $('#rsvpStatus').change(function() {
         $('#rsvpNumber').attr('required', true);
         $('#rsvpNumber').attr('disabled', false);
         $('#rsvpDiet').attr('disabled', false);
-    }
+    };
 });
 
 
-// Form validation and submission to Formspree
+// Form validation (Bootstrap) and submission to Formspree
 var form = $('#rsvpForm');
 form.submit(function(event) {
     // prevent default submission behaviour
     event.preventDefault();
     event.stopPropagation();
 
-    if (form[0].checkValidity() === false) {
-      console.log('form invalid')
+    // Add validation styles
+    form.addClass('was-validated');
+
+    if (form[0].checkValidity() == false) {
+        // wait for better input
+        console.log('form invalid')
     } else {
-        // submit to Formspree & h=show error/success message
+        // submit to Formspree & show error/success message
         console.log('form valid, submitting')
         $.ajax({
-            url: '//formspree.io/john@johnpaton.net',
+            url: '//formspree.io/'+rsvpSubmissionEmail,
             method: 'POST',
-            data: form.serialize(),
+            data: form.serialize(),  // url encoding
             // dataType: 'json',
             // beforeSend: function() {
-            //     $contactForm.append('<div class="alert alert--loading">Sending message…</div>');
             // },
             success: function(data) {
                 // Show the message
@@ -144,11 +176,11 @@ form.submit(function(event) {
                     $(this).attr('disabled', true);
                 });
             },
+
             error: function(err) {
                 // Show the message
                 $('#rsvpError').fadeIn();
-            }
+            },
         });
     };
-    $('#rsvpForm').addClass('was-validated');
 });
